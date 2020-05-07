c73d86c66360e60878de300d0f347632b45bf33c
liamhartley <liamjhartley@yahoo.co.uk>
2020-05-07 08:34:24 +0100

    adding unittests and modifying readme

--- tests/unittest_action.py
+++ tests/unittest_action.py
@@ -6,18 +6,22 @@ from objects import table
 
 
 # TODO more
-class TestActions(unittest.TestCase):
+# Class TestSortPlayers(unittest.TestCase):
+
+# Class TestPostFlop(unittest.TestCase):
+
+class TestPreflop(unittest.TestCase):
     def setUp(self):
         self.test_table = table.TableClass()
         for new_player in range(0, 4):
             self.test_table.table_players.append(player.PlayerClass(name=new_player))
         self.test_table.table_players[new_player].position = 'dealer'
         self.test_table.dealer_first()
-
-    def test_preflop_all_call_and_bb_checks(self):
         self.test_table.current_state = 'pre-flop'
         self.test_table.sort_players_preflop()
         self.test_table.hand_players = self.test_table.table_players
+
+    def test_preflop_all_call_and_bb_checks(self):
 
         # Everyone called, bb checks
         self.test_table.hand_players[0].player_action = 'c'
@@ -27,6 +31,8 @@ class TestActions(unittest.TestCase):
 
         check_hands.preflop_everyone_calls_folds_and_bb_checks(self.test_table)
         self.assertTrue(self.test_table.round_of_betting_complete)
+
+    def test_preflop_calls_folds_and_bb_checks(self):
 
         # People fold and call, bb checks
         self.test_table.hand_players[0].player_action = 'f'
